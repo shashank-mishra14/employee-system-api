e7a740f654db3ebb57f01937e394b127f2ce72ed
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2022-12-05 01:06:04 +0530

    commit

--- src/main/java/com/shashank/employee/services/EmployeeServiceImpl.java
+++ src/main/java/com/shashank/employee/services/EmployeeServiceImpl.java
@@ -1,7 +1,26 @@
 package com.shashank.employee.services;
 
+import com.fasterxml.jackson.databind.util.BeanUtil;
+import com.shashank.employee.entity.EmployeeEntity;
+import com.shashank.employee.model.Employee;
+import com.shashank.employee.repository.EmployeeRepositry;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService{
+    private EmployeeRepositry employeeRepositry;
+
+    public EmployeeServiceImpl(EmployeeRepositry employeeRepositry) {
+        this.employeeRepositry = employeeRepositry;
+    }
+
+    @Override
+    public Employee createEmployee(Employee employee) {
+        EmployeeEntity employeeEntity = new EmployeeEntity();
+
+        BeanUtils.copyProperties(employee, employeeEntity);
+        employeeRepositry.save(employeeEntity);
+        return employee;
+    }
 }
