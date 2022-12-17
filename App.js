da07e57a819653c1ef1f0b0ff4606e3f93bd73cd
Shashank Mishra <98086957+shashank-mishra14@users.noreply.github.com>
2022-12-17 23:05:10 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import AddEmployee from './components/AddEmployee';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+  <>
+   
+  <Navbar/>
+  <AddEmployee/>
+  </>
+
+  );
+
+  
+}
+
+export default App;
