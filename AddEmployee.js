d2837b71cd5cce24874fef44a42001f394ecd944
Shashank Mishra <98086957+shashank-mishra14@users.noreply.github.com>
2022-12-18 18:54:42 +0530

    Add files via upload

--- AddEmployee.js
+++ AddEmployee.js
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router";
+import EmployeeService from "./services/EmployeeService";
 
 const AddEmployee = () => {
 
-    const [employee, setemployee] = useState({
+    const [employee, setEmployee] = useState({
 
         id: "",
         firstName: "",
@@ -10,53 +12,40 @@ const AddEmployee = () => {
         emailId: "",
      });
 
-    return (
-        <div className="flex max-w-2xl mx-auto shadow border-b">
-            <div className="px-8 py-8">
-                <div className="font-thin text-2xl tracking-wider">
-                    <h1>Add New Empolyee</h1>
-                </div>
-                <div className="items-center justify-center h-14 w-full my-4">
-                    <label className="block text-gray-600 text-sm font-normal">First Name</label>
-                    <input 
-                    type="text" 
-                    name="firstName"
-                    value={employee.firstName}
-                    className ="h-10 w-96 border mt-2 px-2 py-2">
+     const navigaye = useNavigate();
 
-                    </input>
-                </div>
-                <div className="items-center justify-center h-14 w-full my-4">
-                    <label className="block text-gray-600 text-sm font-normal">
-                        Last Name
-                        </label>
+     const handleChange = (e) => {
+         const value = e.target.value;
+         setEmployee({ ...employee, [e.target.name]: value});
+     };
 
-                    <input type="text" className ="h-10 w-96 border mt-2 px-2 py-2"></input>
-                </div>
-                <div className="items-center justify-center h-14 w-full my-4">
-                    <label className="block text-gray-600 text-sm font-normal">
-                        Email
-                    </label>
-                    <input type="email" className ="h-10 w-96 border mt-2 px-2 py-2"></input>
+     const saveEmployee = (e) => {
+         e.preventDefault();
+         EmployeeService.saveEmployee(employee)
+         .then((response) => {
+          console.log(response); 
+          navigaye("/employeeList");
 
-                </div>
-                <div className="items-center justify-center h-14 w-full my-4 space-x-4">
-                    <button>
-                    className="rounded text-white font-semibold bg-green-400 hover:bg-green-700 px-2 py-6">
-                    Save
-                    </button>
-                    <button>
-                    className="rounded text-white font-semibold bg-red-400 hover:bg-red-700 px-2 py-6">
-                    Clear
-                    </button>
+         }).catch((error)=>{
+             console.log(error);
+         });
+        };
+         const reset =(e) => {
+             e.preventDefault();
+             setEmployee({
+                id: "",
+                firstName: "",
+                lastName: "",
+                emailId: "",
 
-                </div>
-                
-                
-            </div>
-     </div>
+             });
+         };
+
+     
+    
         
-    )
+        
+    
 }
 
 export default AddEmployee
